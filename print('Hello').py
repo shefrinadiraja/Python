05a7d9f7e70075474f357b2abdbf761832230f33
shefrinadiraja <shefrinadiraja@yahoo.com>
2025-05-09 07:13:12 +0400

    first file is made

---
+++ print('Hello').py
@@ -0,0 +1,36 @@
+print('Hello')#
+
+x=10#
+print(x)
+
+name="shefrin"#
+print(name)
+
+name='shefrin'
+age=10
+height=7.9
+human=True
+
+print (name,age,height,human)#
+
+print ('name is',name, 'height is',height, 'age is', age, 'human is',human)#
+
+print ('name is %s, height is %f, age is %d, human is %r' %(name,height,age,human))#
+
+print ('name is {}, height is {}, age is {}, human is {}'.format(name,height,age,human))#
+
+
+X=20
+Y=10
+Sum=X+Y
+Difference=X-Y
+Product=X*Y
+Division=X/Y
+
+print (Sum,Difference,Product,Division)#
+print ('Sum is',Sum, 'Difference is',Difference, 'Product is',Product, 'Division is',Division)#
+
+
+
+
+
